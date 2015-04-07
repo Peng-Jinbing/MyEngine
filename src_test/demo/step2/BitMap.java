c31a3d272ad102abcca1588e605913592be1636a
jinbing <jinbing.peng@yahoo.com>
2015-04-07 13:26:22 -0400

    Added screen(for render), ticker(for update) and main loop

---
+++ src_test/demo/step2/BitMap.java
@@ -0,0 +1,26 @@
+package demo.step2;
+
+public class BitMap {
+
+	protected int width;
+	protected int height;
+	protected int[] pixels;
+
+	public BitMap(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.pixels = new int[width * height];
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int[] getPixels() {
+		return pixels;
+	}
+}
---
+++ src_test/demo/step2/Ticker.java
@@ -0,0 +1,8 @@
+package demo.step2;
+
+public class Ticker {
+
+	public void tick() {
+	}
+
+}
