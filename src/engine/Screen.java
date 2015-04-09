5c48f7a644aec8ab813cf1c787d3ba4245c4ff05
jinbing <jinbing.peng@yahoo.com>
2015-04-09 17:25:02 -0400

    step4 6 clean the screen for each frame.

--- src/engine/Screen.java
+++ src/engine/Screen.java
@@ -8,24 +8,25 @@ public class Screen extends Texture {
 	}
 
 	public void render(Ticker ticker) {
+		for (int i = 0; i < width * height; i++) {
+			this.pixels[i]=0;
+		}
+		
 		// int textureWidth = 160;
 		// int textureHeight = 120 ;
 		int textureWidth = 64;
 		int textureHeight = 64;
-
-		Texture testGraphics = new Texture(textureWidth, textureHeight);
-		int[] testPixels = testGraphics.getPixels();
-		Random r = new Random();
-		for (int i = 0; i < textureHeight; i++) {
-			for (int j = 0; j < textureWidth; j++) {
-				testPixels[i * textureWidth + j] = r.nextInt();
-			}
-		}
-
 		int x1 = (this.width - textureWidth) / 2;
 		int y1 = (this.height - textureHeight) / 2;
 		int x2 = x1 + textureWidth - 1;
 		int y2 = y1 + textureHeight - 1;
+
+		Texture testGraphics = new Texture(textureWidth, textureHeight);
+		int[] testPixels = testGraphics.getPixels();
+		Random r = new Random();
+		for (int i = 0; i < textureHeight*textureWidth; i++) {
+			testPixels[i] = r.nextInt()* ((int)(r.nextInt(5)/4));
+		}
 
 		int speedFactor = 2000;
 
@@ -33,6 +34,7 @@ public class Screen extends Texture {
 		int yd = (int) (Math.cos(System.currentTimeMillis() % speedFactor / Double.valueOf(speedFactor) * Math.PI * 2) * 40);
 
 		this.draw(testGraphics, x1 + xd, y1 + yd);
+		
 
 		Line lineLeft = new Line(x1, y1, x1, y2, 255 << 8);
 		Line lineBottom = new Line(x1, y2, x2, y2, 255 << 8);
@@ -68,7 +70,10 @@ public class Screen extends Texture {
 					continue;
 				}
 
-				super.pixels[posY * width + posX] = texturePixels[i * textureWidth + j];
+				int srcPixel = texturePixels[i * textureWidth + j];
+				if(srcPixel>0){
+					super.pixels[posY * width + posX] = srcPixel;
+				}
 			}
 		}
 	}
