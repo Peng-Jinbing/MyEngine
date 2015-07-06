9668223da9ea8793968992fd57b82119d6f260b7
jinbing <jinbing.peng@yahoo.com>
2015-07-06 14:18:38 -0400

    added imageLoader Class

---
+++ src_test/demo/step2/ImageLoader.java
@@ -0,0 +1,29 @@
+package demo.step2;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import demo.step2.Bitmap;
+
+
+public class ImageLoader {
+	public static Bitmap sprites = load("/tex/sprites.png");
+
+	private static Bitmap load(String filename) {
+		try {
+			URL url = ImageLoader.class.getResource(filename);
+			BufferedImage img = ImageIO.read(url);
+			int w = img.getWidth();
+			int h = img.getHeight();
+			Bitmap result = new Bitmap(w, h);
+			int[] pixels = result.getPixels();
+			img.getRGB(0, 0, w, h, pixels, 0, w);
+			return result;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
