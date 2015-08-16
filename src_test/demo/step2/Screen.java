e364b7ca484b3bf7578914dcf6670e78468890a5
peng <jinbing.peng@yahoo.com>
2015-08-16 00:42:07 -0400

    add transpant

--- src_test/demo/step2/Screen.java
+++ src_test/demo/step2/Screen.java
@@ -6,33 +6,58 @@ public class Screen extends Bitmap {
 	}
 
 	public void render(Ticker ticker, Bitmap bitmap) {
-		this.putBitmap(bitmap, 0, 0);
+		this.putBitmap(bitmap, 0, 0, false);
 	}
 
-	public void putBitmap(Bitmap bitmap, int offX, int offY) {
+	public void putBitmap(Bitmap bitmap, int offX, int offY, boolean transparent) {
 		int w = bitmap.getWidth();
 		int h = bitmap.getHeight();
 		int[] p = bitmap.getPixels();
 		int[] screenPixels = super.pixels;
 
 		int posX = 0, posY = 0;
-		for (int y = 0; y < h; y++) {
-			posY = offY + y;
-			if (posY < 0 || posY >= super.height) {
-				continue;
-			}
-
-			int screenRowOffset = posY * width;
-			int bitmapRowOffset = y * w;
-
-			for (int x = 0; x < w; x++) {
-				posX = offX + x;
-				if (posX < 0 || posX >= super.width) {
+		int pixel;
+		if (transparent) {
+			for (int y = 0; y < h; y++) {
+				posY = offY + y;
+				if (posY < 0 || posY >= super.height) {
 					continue;
 				}
 
-				screenPixels[screenRowOffset + posX] = p[bitmapRowOffset + x];
+				int screenRowOffset = posY * width;
+				int bitmapRowOffset = y * w;
+
+				for (int x = 0; x < w; x++) {
+					posX = offX + x;
+					if (posX < 0 || posX >= super.width) {
+						continue;
+					}
+					pixel= p[bitmapRowOffset + x];
+					if (pixel != 0) {
+						screenPixels[screenRowOffset + posX] = pixel;
+					}
+				}
 			}
+		} else {
+			for (int y = 0; y < h; y++) {
+				posY = offY + y;
+				if (posY < 0 || posY >= super.height) {
+					continue;
+				}
+
+				int screenRowOffset = posY * width;
+				int bitmapRowOffset = y * w;
+
+				for (int x = 0; x < w; x++) {
+					posX = offX + x;
+					if (posX < 0 || posX >= super.width) {
+						continue;
+					}
+
+					screenPixels[screenRowOffset + posX] = p[bitmapRowOffset + x];
+				}
+			}
+
 		}
 	}
 	
